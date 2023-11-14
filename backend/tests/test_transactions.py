427d6707cd0a81c6d927bfdb54fa6c5024e7d260
Hyojin Kwak <hyojinkwak@Hyojins-MBP.lan>
2023-11-14 15:42:44 -0800

    test wip

--- backend/tests/test_transactions.py
+++ backend/tests/test_transactions.py
@@ -5,8 +5,18 @@ from ..main import app
 client = TestClient(app)
 
 
+def login_user():
+    res = client.post("/auth/login", data={"username": "hyojin", "password": "12345"})
+    return res.json().get("access_token")
+
+
 def test_create_transaction():
-    response = client.post("/transaction")
+    token = login_user()
+    response = client.post("/transaction", data={
+                                                 "card_number": "4111112014267661",
+                                                 "amt": 123,
+                                                 "card_type": "debit"},
+                           headers={"Authorization": "Bearer" + token})
     assert response.status_code == 201
 
 
