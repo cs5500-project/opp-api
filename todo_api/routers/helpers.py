c18c196bca21518e09c843b4e51232e8be71b48c
Xiaolin Zhan <zhanxl@live.com>
2023-12-04 19:55:25 -0800

    remove todo-app

--- todo_api/routers/helpers.py
+++
@@ -1,6 +0,0 @@
-from starlette.exceptions import HTTPException
-
-
-def check_user_authentication(user):
-    if user is None:
-        raise HTTPException(status_code=401, detail='Authentication Failed')
